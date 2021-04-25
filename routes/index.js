a6ce4143e611bb0ffbf1ac8a94e5ed4c115104ce
scmstr <scmstr@hotmail.com>
2021-04-25 16:22:34 -0700

    security

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const Orders = require("../Orders");
 // edited to include my non-admin, user level account and PW on mongo atlas
 // and also to include the name of the mongo DB that the collection
 const dbURI =
-"mongodb+srv://Admin:headcrab@jessieclustertest.ecxbf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+"mongo connect here";
 
 
 // Make Mongoose use `findOneAndUpdate()`. Note that this option is `true`
